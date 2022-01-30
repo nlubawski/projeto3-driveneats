c250226fe47dac783b2b090b43a2b1296667db50
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-01-29 23:05:36 -0300

    implementada a funcao selecionar para as bebidas

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function selecionar(classeSelecionada){
-    const selecionado = document.querySelector(".selecionado")
+function selecionarPrato(classeSelecionada){
+    const selecionadoP = document.querySelector(".selecionado")
 
-    if (selecionado  != null){
-        selecionado.classList.remove("selecionado")
+    if (selecionadoP  != null){
+        selecionadoP.classList.remove("selecionado")
     }
 
     const pratoEscolhido = document.querySelector("." + classeSelecionada)
     pratoEscolhido.classList.add("selecionado")
+    
+}
+
+function selecionarBebida(classeSelecionada){
+    const selecionadoB = document.querySelector(".selecionado")
+
+    if (selecionadoB  != null){
+        selecionadoB.classList.remove("selecionado")
+    }
+
+    const bebidaEscolhida = document.querySelector("." + classeSelecionada)
+    bebidaEscolhida.classList.add("selecionado")
     
 }
